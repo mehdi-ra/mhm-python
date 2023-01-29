0ee84b83ab1e2e6033b7552cc188bcfd3327d5b9
Mehdi Rahimi <83763144+me-dira@users.noreply.github.com>
2023-01-29 04:43:53 +0000

    chore: added basics note book

--- algorithms/linear-search.py
+++
@@ -1,8 +0,0 @@
-# Write a function like: search(value, []) and search
-# Inside the given List, Tuple, Set or Array.
-
-def search(value: str, items = []):
-  print('You are searching:', value)
-
-
-search('Hello')
---
+++ algorithms/linear_search.py
@@ -0,0 +1,13 @@
+"""
+We are going to implement linear search method below:
+"""
+
+"""
+"""
+def search(value: str, items = []):
+
+
+def search(value: str, items=[]):
+    print('You are searching:', value, items)
+
+search('Hello')
